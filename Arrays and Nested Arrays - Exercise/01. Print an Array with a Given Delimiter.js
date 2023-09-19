2f3cbcda3c3c7efc4a7cc4ae4dde3e6c92611af4
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2023-09-19 23:02:02 +0300

    new

---
+++ Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter.js
@@ -0,0 +1,25 @@
+// The input comes as two parameters – an array of strings and a string. The second parameter is the delimiter.
+// The output is the elements of the array, printed on the console, each element separated from the others by the given delimiter.
+
+function printArr(array, string) {
+
+    let delimiter = string
+    let result = []
+   
+    array.forEach(element => {
+        result.push(element)
+        result.push(delimiter)
+    });
+   
+    let junk = result.pop()
+    console.log(result.join(''));
+
+}
+
+printArr(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'
+)
